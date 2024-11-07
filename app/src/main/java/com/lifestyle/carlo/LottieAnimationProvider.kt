a276851fdc6b1cbad5c4f34f8c1e143be5a896da
yogas <yogasanjeevarayan@zed.digital>
2024-11-07 14:17:37 +0530

    jitpack added

--- app/src/main/java/com/lifestyle/carlo/LottieAnimationProvider.kt
+++ app/src/main/java/com/lifestyle/carlo/LottieAnimationProvider.kt
@@ -2,6 +2,13 @@ package com.lifestyle.carlo
 
 import android.content.Context
 import com.airbnb.lottie.LottieAnimationView
+import com.konovalov.vad.silero.Vad
+import com.konovalov.vad.silero.VadSilero
+import com.konovalov.vad.silero.config.FrameSize
+import com.konovalov.vad.silero.config.Mode
+import com.konovalov.vad.silero.config.SampleRate
+
+
 
 class LottieAnimationProvider(private val context: Context) {
     fun getAnimationView(animationResId: Int): LottieAnimationView {
